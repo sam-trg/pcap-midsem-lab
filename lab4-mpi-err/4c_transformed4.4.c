68538495265ab084e4c02751333bff219dd5d04c
Samarth Asija <90459209+sam-trg@users.noreply.github.com>
2025-04-03 10:35:00 +0530

    Update 4c_transformed4.4.c

--- lab4-mpi-err/4c_transformed4.4.c
+++ lab4-mpi-err/4c_transformed4.4.c
@@ -3,11 +3,17 @@
 
 int main(int argc, char* argv[]) {
     int rank, size;
-    int matrix[4][4], result[4][4];
+    int matrix[4][4], result[4];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size != 4) {
+        if (rank == 0) printf("Error: This program requires 4 processes.\n");
+        MPI_Finalize();
+        return 1;
+    }
 
     if (rank == 0) {
         printf("Enter 4x4 matrix:\n");
@@ -16,19 +22,19 @@ int main(int argc, char* argv[]) {
                 scanf("%d", &matrix[i][j]);
     }
 
-    MPI_Scatter(matrix, 4, MPI_INT, result[rank], 4, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(matrix, 4, MPI_INT, result, 4, MPI_INT, 0, MPI_COMM_WORLD);
 
     for (int i = 0; i < 4; i++) {
-        result[rank][i] += rank;
+        result[i] += rank;
     }
 
-    MPI_Gather(result[rank], 4, MPI_INT, result, 4, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(result, 4, MPI_INT, matrix, 4, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         printf("Resultant Matrix:\n");
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                printf("%d ", result[i][j]);
+                printf("%d ", matrix[i][j]);
             }
             printf("\n");
         }
