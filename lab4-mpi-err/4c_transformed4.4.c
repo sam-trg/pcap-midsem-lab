519f8ae16eb46a87649c8f62d6c321a811155ce4
Samarth Asija <90459209+sam-trg@users.noreply.github.com>
2025-04-03 11:05:03 +0530

    Final fixed 4c

--- lab4-mpi-err/4c_transformed4.4.c
+++ lab4-mpi-err/4c_transformed4.4.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]){
     int rank, size;
-    int matrix[4][4], result[4];
+    int matrix[4][4];
+    int row[4], prefix[4];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size != 4) {
-        if (rank == 0) printf("Error: This program requires 4 processes.\n");
+    if(size != 4){
+        if(rank == 0)
+            printf("This program requires 4 processes.\n");
         MPI_Finalize();
         return 1;
     }
 
-    if (rank == 0) {
+    if(rank == 0){
         printf("Enter 4x4 matrix:\n");
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < 4; j++)
                 scanf("%d", &matrix[i][j]);
     }
 
-    MPI_Scatter(matrix, 4, MPI_INT, result, 4, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(matrix, 4, MPI_INT, row, 4, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scan(row, prefix, 4, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Gather(prefix, 4, MPI_INT, matrix, 4, MPI_INT, 0, MPI_COMM_WORLD);
 
-    for (int i = 0; i < 4; i++) {
-        result[i] += rank;
-    }
-
-    MPI_Gather(result, 4, MPI_INT, matrix, 4, MPI_INT, 0, MPI_COMM_WORLD);
-
-    if (rank == 0) {
+    if(rank == 0){
         printf("Resultant Matrix:\n");
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++)
                 printf("%d ", matrix[i][j]);
-            }
             printf("\n");
         }
     }
